13817b9294a34c250d36a2048a78a6ce102a2adb
Erling Neerland Lone <erling.neerland.lone@dnvgl.com>
2019-03-22 13:23:24 +0100

    Uploaded app modules/scripts

---
+++ qats/app/logger.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Module implementing a logger class
+
+@author: perl
+"""
+import logging
+from PyQt5.QtWidgets import QTextBrowser
+
+
+class QLogger(logging.Handler):
+    """
+    Logger handler
+    """
+    def __init__(self, parent):
+        super().__init__()
+        self.widget = QTextBrowser(parent)
+        self.widget.setReadOnly(True)
+
+    def emit(self, record):
+        """
+        Append logger record to text browser
+
+        Parameters
+        ----------
+        record : str
+            logger record
+
+        """
+        record = self.format(record)
+        self.widget.append(record)
---
+++ qats/app/widgets.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Module with custom widgets
+
+@author: perl
+"""
+from PyQt5.QtWidgets import QTabWidget
+
+
+class CustomTabWidget (QTabWidget):
+    """
+    Custom tab widget to enable closing tabs
+    """
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setTabsClosable(True)
+        self.tabCloseRequested.connect(self.close_tab)
+
+    def close_tab(self, index):
+        """
+        Close tab by index
+        """
+        current_widget = self.widget(index)
+        current_widget.deleteLater()
+        self.removeTab(index)
