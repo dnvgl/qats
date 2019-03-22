648d738f0074621a7f83fee875b0502e36b4f810
Erling Neerland Lone <erling.neerland.lone@dnvgl.com>
2019-03-22 13:26:42 +0100

    Uploaded docs/source/examples files

---
+++ docs/source/examples/rainflow.py
@@ -0,0 +1,19 @@
+"""
+Example of fetching a single time series from the time series database class and count the cycles
+"""
+import os
+from qats import TsDB
+
+db = TsDB()
+
+# locate time series file
+file_name = os.path.join("..", "..", "..", "data", "mooring.ts")
+
+# load time series from file
+db.load([file_name])
+
+# get a single the time series from the database
+ts = db.get_ts(name="surge")    # ts is now a TimeSeries object
+
+# count the cycles using the rainflow algorithm, grouping the cycles to 256 equidistant bins
+cycles = ts.rfc(nbins=256)  # cycles is a sorted list of tuples of cycle magnitude versus count
