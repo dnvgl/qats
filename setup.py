7acce8011f68eaf87bb734d370a2de8e7b143837
tovop <per.voie@gmail.com>
2019-05-01 16:29:31 +0200

    Updated packaging script.
    Will deploy the package as a wheel distribution. This works with both
    conda environments and python virtual environments. It solves issues
    with entry points, inconsistent package naming on conda and pypi (pyqt)
    etc.

--- setup.py
+++ setup.py
@@ -4,50 +4,49 @@ Setup script for building and installing package and building html documentation
 """
 from setuptools import setup, find_packages
 from setuptools_scm import get_version
-from sphinx.setup_command import BuildDoc
 
 # Set the package version according to semantic versioning scheme
 version = get_version()
 
 setup(
+    # package data
     name="qats",
     version=version,
     packages=find_packages(exclude=("test",)),
     package_data={
         "qats.app": ["qats.ico"],
     },
+    python_requires="~=3.6",
+    setup_requires=["setuptools_scm>=3,<4"],
+    install_requires=[
+        "setuptools_scm>=3,<4",
+        "openpyxl>=2,<3",
+        "numpy>=1,<2",
+        "scipy>=1,<2",
+        "matplotlib>=3,<4",
+        "h5py>=2.7,<3",
+        "PyQt5>=5.6,<6",
+        "pandas>=0.24,<1",
+        "pywin32; platform_system == 'Windows'"
+    ],
     entry_points={
-        'console_scripts': ['qats = qats.cli:main'],
-        "gui_scripts": ['qats-app = qats.cli:launch_app']
-    },
-    cmdclass={
-        "build_docs": BuildDoc      # directive that builds the sphinx documentation
-    },
-    command_options={
-        "build_docs": {
-            "version": ("setup.py", version),
-            "source_dir": ("setup.py", "docs/source"),
-            "build_dir": ("setup.py", "docs/_build"),
-            "builder": ("setup.py", "html")
-        }
+        "console_scripts": ["qats = qats.cli:main"],
+        "gui_scripts": ["qats-app = qats.cli:launch_app"]
     },
     zip_safe=True,
 
     # meta-data
     author="Per Voie & Erling Lone",
     description="Library for efficient processing and visualization of time series.",
-    long_description="A python library for efficient processing and visualizing time series. The libary provides a "
-                     "TimeSeries class and TsDB class holding several TimeSeries objects. And functions for "
-                     "estimation of power spectral density, fitting of probability distributions, sample statistics, "
-                     "extreme value statistics and rainflow cycle counting.",
+    long_description="The libary provides a TimeSeries class and TsDB class holding several TimeSeries objects. And "
+                     "functions for estimation of power spectral density, fitting of probability distributions, sample "
+                     "statistics, extreme value statistics and rainflow cycle counting.",
     license="MIT",
     url="https://github.com/dnvgl/qats",
-    download_url="",
+    download_url="https://pypi.org/project/qats/",
     project_urls={
-        "Source Code": "https://github.com/dnvgl/qats",
         "Issue Tracker": "https://github.com/dnvgl/qats/issues",
-        "Documentation": "",
-        "Conda channel": "",
+        "Documentation": "https://readthedocs.org/projects/qats/",
     }
 
 )
