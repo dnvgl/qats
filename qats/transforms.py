2590cd4d1a3539f9926bdb937d49554e3b65f444
eneelo <erling.neerland.lone@dnvgl.com>
2019-07-10 11:53:46 +0200

    First commit of new module 'transforms' (wip)

---
+++ qats/transforms.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+"""
+Functions for time series transformations, numerical time differentiation, etc.
+"""
+import numpy as np
+from numpy import sin, cos, radians as rad
+
+
