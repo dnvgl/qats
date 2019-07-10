8aa4208422e8b745a1f30023547f24ab9a0cd658
eneelo <erling.neerland.lone@dnvgl.com>
2019-07-10 13:51:51 +0200

    Reverted introduction of transforms.py

--- qats/transforms.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-"""
-Functions for time series transformations, numerical time differentiation, etc.
-"""
-import numpy as np
-from numpy import sin, cos, radians as rad
-
-
