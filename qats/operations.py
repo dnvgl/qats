b3e38b02e440a311b158750791a0b9ce55c054c8
eneelo <erling.neerland.lone@dnvgl.com>
2019-07-12 12:27:19 +0200

    operations.py: velocity() and acceleration() introduced

--- qats/operations.py
+++ qats/operations.py
@@ -70,3 +70,93 @@ def transform_motion(motion, newref, rotunit="deg"):
     xyz += motion[:3, :]
 
     return xyz
+
+
+def acceleration(x, t):
+    """
+    Numerical time differentiation to obtain acceleration of signal(s).
+
+
+    Parameters
+    ----------
+    x : list or np.array
+        Signal(s) to perform time derivation on. See requirements given for function :func:`velocity`.
+    t : float or np.array
+        Time step or time array.
+
+    Returns
+    -------
+    acc : np.array
+        Acceleration of input signal(s); same shape as input.
+
+    Notes
+    -----
+    In practice, acceleration is calculated by calling function `velocity` twice.
+
+    See Also
+    --------
+    velocity
+    np.gradient
+    """
+    acc = x[:]  # copy input signals
+    for _ in range(2):
+        acc = velocity(acc, t)
+    return acc
+
+
+def velocity(x, t):
+    """
+    Numerical time differentiation to obtain velocity of signal(s).
+
+    Parameters
+    ----------
+    x : list or np.array
+        Signal to perform time derivation on. If a 2-D array is given, it is required to be of shape (n, nt), where `n`
+        is number of signals and `nt` is number of time steps. Time differentiation is then performed for each of the
+        signals independently.
+    t : float or np.array
+        Time step or time array.
+
+    Returns
+    -------
+    vel : np.array
+        Velocity of input signal(s); same shape as input.
+
+    Notes
+    -----
+    Velocity is calculated using second order central differences, with first order backwards and forward differences
+    at start/end of signals.
+    See `numpy.gradient <https://docs.scipy.org/doc/numpy/reference/generated/numpy.gradient.html>`_.
+
+
+    See Also
+    --------
+    numpy.gradient
+
+    Notes
+    -----
+    """
+    # check input signal(s)
+    x = np.asarray(x)
+    assert x.ndim in (1, 2), "Input signal 'x' must be 1- or 2-D array"
+    # check specified time
+    if isinstance(t, float):
+        # time step is specified
+        pass
+    else:
+        # assume time array is specified -> check that it matches the number of time steps in input signals
+        t = np.asarray(t)   # ensure t is numpy array
+        nt = x.shape[-1]    # number of time steps
+        assert t.size == nt, "If time array is specified, it must match number of time steps in input signal(s)"
+
+    if x.ndim == 1:
+        axis = None
+    else:
+        # more than one signal is given -> perform time differentation along axis 1
+        axis = 1
+
+    # time differentiation
+    vel = np.gradient(x, t, axis=axis)
+
+    return vel
+
