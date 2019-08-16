db0531791ac8ab960656a36489e19fecb76ce9e1
eneelo <erling.neerland.lone@dnvgl.com>
2019-08-16 12:58:16 +0200

    test_fatigue_sn.py: new test module for fatigue.sn module (WIP)

---
+++ test/test_fatigue_sn.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Module for testing module fatigue.sn
+"""
+
+import unittest
+import numpy as np
+from qats.fatigue.sn import SNCurve, dcalc_sn, dcalc_sn_weib
+
+# todo: include tests for SNCurve class (initiation, relevant methods, ++)
+# todo: include test for dcalc_sn()
+# todo: include test for dcacl_sn_weib()
+
+
+class TestFatigueSn(unittest.TestCase):
+    def setUp(self):
+        """
+        Common setup for all tests
+        """
+        pass
+
