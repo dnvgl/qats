bfef411d16af5ef2603067da26bbca10fcee448a
tovop <per.voie@gmail.com>
2019-08-19 13:48:20 +0200

    Updated cycle counting examples.
    Resolves #34

--- docs/source/examples/rainflow.py
+++ docs/source/examples/rainflow.py
@@ -1,5 +1,5 @@
 """
-Example of fetching a single time series from the time series database class and count the cycles
+Example on working with cycle range and range-mean distributions.
 """
 import os
 from qats import TsDB
@@ -10,13 +10,25 @@ file_name = os.path.join("..", "..", "..", "data", "simo_p_out.ts")
 # load time series directly on db initiation
 db = TsDB.fromfile(file_name)
 
-# plot the cycle range distribution (range versus count) grouped in 100 bins
-db.plot_cycle_range(names='tension*', n=100)
-
-# plot the cycle range-mean distribution grouped in 100 cycle range bins
-db.plot_cycle_rangemean(names='tension*', n=100)
-
-# plot the cycle range-mean distribution as a 3D surface.
+# fetch one of the time series from the db
 ts = db.get(name='tension_2_qs')
+
+# plot its cycle ranges as bar diagram
+ts.plot_cycle_range(n=100)
+
+# plot its cycle-range-mean distribution as scatter
+ts.plot_cycle_rangemean(n=100)
+
+# ... or as a 3D surface.
 ts.plot_cycle_rangemean3d(nr=25, nm=25)
 
+# you can also collect the cycle range-mean and count numbers (see TimeSeries.rfc and TimeSeries.get for options)
+cycles = ts.rfc()
+ranges, means, counts = zip(*cycles)    # unpack to separate lists if you prefer
+
+# The TsDB class also has similar methods to ease comparison
+# compare cycle range distribution (range versus count) grouped in 100 bins
+db.plot_cycle_range(names='tension*', n=100)
+
+# compare cycle range-mean distribution grouped in 100 bins
+db.plot_cycle_rangemean(names='tension*', n=100)
