2ffd6571f0564ecf9a1bc2edb8d21154778357af
eneelo <erling.neerland.lone@dnvgl.com>
2019-08-27 13:55:37 +0200

    test_readers.py: all files are tested before potential failure is raised

--- test/test_readers.py
+++ test/test_readers.py
@@ -7,6 +7,7 @@ generate any exceptions.
 from qats import TsDB
 import unittest
 import os
+import sys
 
 # todo: add test class for matlab
 
@@ -18,12 +19,15 @@ class TestAllReaders(unittest.TestCase):
         self.data_directory = os.path.join(os.path.dirname(__file__), '..', 'data')
         # file name, number of (time series) keys
         self.files = [
+            # sima h5 files
             ("results_SIMA341.h5", 768),
             ("results_SIMA35.h5", 774),
             ("results_SIMA36.h5", 774),
             ("results_SIMA37dev.h5", 774),
+            # sima bin files (riflex)
             ("n_elmfor.bin", 27),
             ("n_elmtra.bin", 162),
+            # direct access files
             ("mooring.ts", 14),
             ("simo_p.ts", 22),
             ("simo_p_out.ts", 22),
@@ -32,20 +36,37 @@ class TestAllReaders(unittest.TestCase):
             ("simo_trans.ts", 12),
             ("simo_n.tda", 6),
             ("decay.tda", 6),
+            # csv files
             ("example.csv", 6),
+            # # ascii files
             ("model_test_data.dat", 39),
         ]
 
     def test_correct_number_of_timeseries(self):
+        failed = []
         for filename, nts in self.files:
             db = TsDB.fromfile(os.path.join(self.data_directory, filename))
-            self.assertEqual(nts, db.n, f"Failed to identify correct number of time series on '{filename}'")
+            if not nts == db.n:
+                failed.append(f"{filename} ({nts} != {db.n})")
+        self.assertTrue(len(failed) == 0,
+                        f"Failed to identify correct number of time series on {len(failed)} file(s):\n   *** " +
+                        f"\n   *** ".join(failed))
 
     def test_correct_timeseries_size(self):
+        failed = []
         for filename, _ in self.files:
-            db = TsDB.fromfile(os.path.join(self.data_directory, filename))
-            ts = db.get(ind=0)  # should not fail
-            self.assertTrue(ts.t.size > 1 and ts.t.size == ts.x.size, f"Reading of time series from '{filename}' failed")
+            try:
+                db = TsDB.fromfile(os.path.join(self.data_directory, filename))
+                ts = db.get(ind=0)  # should not fail
+                self.assertTrue(ts.t.size > 1 and ts.t.size == ts.x.size,
+                                f"Did not read time series correctly (t.size = {ts.t.size}, x.size = {ts.x.size})")
+            except Exception:
+                exctype, excvalue, _ = sys.exc_info()
+                exctypestr = str(exctype).lstrip("<class '").rstrip("'>")  # e.g. <class 'IndexError'>  =>  IndexError
+                failed.append(f"{filename}: {exctypestr}: {excvalue}")
+        self.assertTrue(len(failed) == 0,
+                        f"Failed to read time series from {len(failed)} file(s):\n   *** " +
+                        f"\n   *** ".join(failed))
 
 
 if __name__ == '__main__':
