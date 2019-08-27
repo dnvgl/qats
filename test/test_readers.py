288e605dad288a10f102b63d280482d4884d9b9d
tovop <per.voie@gmail.com>
2019-08-27 08:55:43 +0200

    Added tests for direct access file reader.
    Resolves #40

--- test/test_readers.py
+++ test/test_readers.py
@@ -8,8 +8,6 @@ from qats import TsDB
 import unittest
 import os
 
-# todo: add test class for csv
-# todo: add test class for direct_access
 # todo: add test class for matlab
 # todo: add test class for sima (.bin)
 # todo: add test class for other (ascii)
@@ -56,7 +54,35 @@ class TestReadersCSV(unittest.TestCase):
     def test_correct_number_of_timeseries(self):
         for filename, nts in self.files:
             db = TsDB.fromfile(os.path.join(self.data_directory, filename))
-            self.assertEqual(db.n, nts, f"Failed to identify correct number of time series on '{filename}'")
+            self.assertEqual(nts, db.n, f"Failed to identify correct number of time series on '{filename}'")
+
+    def test_correct_timeseries_size(self):
+        for filename, _ in self.files:
+            db = TsDB.fromfile(os.path.join(self.data_directory, filename))
+            ts = db.get(ind=0)  # should not fail
+            self.assertTrue(ts.t.size > 1 and ts.t.size == ts.x.size, f"Reading of time series from '{filename}' failed")
+
+
+class TestReadersDirectAccess(unittest.TestCase):
+    def setUp(self):
+        # the data directory used in the test relative to this module
+        # necessary to do it like this for the tests to work both locally and in virtual env
+        self.data_directory = os.path.join(os.path.dirname(__file__), '..', 'data')
+        # time series arranged by column on .csv files
+        self.files = [
+            # file name, number of (time series) keys
+            ("mooring.ts", 14),
+            ("simo_p.ts", 22),
+            ("simo_p_out.ts", 22),
+            ("simo_r1.ts", 22),
+            ("simo_r2.ts", 22),
+            ("simo_trans.ts", 12),
+        ]
+
+    def test_correct_number_of_timeseries(self):
+        for filename, nts in self.files:
+            db = TsDB.fromfile(os.path.join(self.data_directory, filename))
+            self.assertEqual(nts, db.n, f"Failed to identify correct number of time series on '{filename}'")
 
     def test_correct_timeseries_size(self):
         for filename, _ in self.files:
