c9e775958506019fe26b15f303131af0d294b6c0
Einar <ebg@sevassp.com>
2019-11-02 14:21:55 +0100

    Crude implementation of Matlab .mat v7.3 reader. Committing to rebase on version 4.3.0

--- qats/readers/matlab.py
+++ qats/readers/matlab.py
@@ -1,42 +1,50 @@
 """
-Readers for binary time series files exported from MATLAB
+Readers for Matlab v >= 7.3 binary time series files, which ar in HDF5 format
+Built on Sintef Ocean's model test export format
 """
-import numpy as np
+import os
+import hdf5storage
 import fnmatch
-from datetime import datetime, timedelta
-from scipy.io import loadmat
+import numpy as np
 
 
-# TODO: Rename this module matlab_v72 when we have a use case on v. 7.3 or newer. The new module should be named matlab
-
-
-def read_names(path):
+def read_names(path, verbose=False):
     """
-    Read time series names from MATLAB .mat file.
+    Extracts time series names for `mat` data sets containing (or; interpreted as) time series from h5 based mat-file
+    exported from Matlab v=>7.3.
 
     Parameters
     ----------
-    path : str
-        MAT file path (relative or absolute)
+    path: str
+        Path (relative or absolute) to h5-file
+    verbose: bool, optional
+        If True, print info.
 
     Returns
     -------
-    str
-        Name of the time array
     list
-        Time series names
-
-    Examples
-    --------
-    >>> tname, names = read_names('data.mat')
-
-    Notes
-    -----
-    This code works for Matlab version < 7.3. For version >= 7.3, .mat is on hdf5 format
-    <http://pyhogs.github.io/reading-mat-files.html>.
+        List of time series names (datasets)
     """
-    mat = loadmat(path)
-    names = list(mat.keys())  # make list, since type dict_keys does not support remove()
+    if not os.path.isfile(path):
+        raise FileNotFoundError("file not found: %s" % path)
+
+    if verbose:
+        print('Identifying datasets on %s ...' % path)
+
+    names = []  # list of dataset names/keys (only for data sets that are time series)
+
+    # hdfstorage
+    mat = hdf5storage.loadmat(path)
+    shape = mat['chan_names'].shape
+
+    if shape[0] == 1 and shape[1] >= 1:
+        for name in mat['chan_names'][0]:
+            names.append((str(name[0][0])))
+    elif shape[0] >= 1 and shape[1] == 1:
+        for name in mat['chan_names']:
+            names.append((str(name[0][0][0])))
+    else:
+        raise KeyError("File does not contain a channel name vector: %s" % path)
 
     # identify time key, check that there is only one
     _tn = fnmatch.filter(names, '[Tt]ime*')
@@ -45,74 +53,67 @@ def read_names(path):
     elif len(_tn) > 1:
         raise KeyError("Duplicate time vectors on file: %s" % path)
 
-    # filter names, keep only np.ndarrays of same size as time array
-    timename = _tn[0]
-    tsize = mat[timename].size
-    names = [k for k, v in mat.items() if (isinstance(v, np.ndarray) and v.size == tsize)]
-
     # remove time key from list of time series
+    timename = _tn[0]
     names.remove(timename)
+
+    print(names)
 
     return timename, names
 
 
 def read_data(path, names, verbose=False):
     """
-    Read time-series from MATLAB .mat file.
+        Read time-series from MATLAB .mat v => 7.3 file.
 
-    Parameters
-    ----------
-    path : str
-        Path (relative or absolute) to the time series file.
-    names : list
-        Names of the requested time series incl. the time array itself
-    verbose : bool, optional
-        Increase verbosity
+        Parameters
+        ----------
+        path : str
+            Path (relative or absolute) to the time series file.
+        names : list
+            Names of the requested time series incl. the time array itself
+        verbose : bool, optional
+            Increase verbosity
 
-    Returns
-    -------
-    dict
-        Time and data
+        Returns
+        -------
+        dict
+            Time and data
 
-    Examples
-    --------
-    >>> tname, names = read_names('data.mat')
-    >>> data = read_data('data.mat')
-    >>> t = data[tname]   # time
-    >>> x1 = data[names[0]]  # first data series
-    """
+        Examples
+        --------
+        >>> tname, names = read_names('data.mat')
+        >>> data = read_data('data.mat', names)
+        >>> t = data[tname]   # time
+        >>> x1 = data[names[0]]  # first data series
+        """
     if verbose:
         print('Reading %s ...' % path)
 
-    data = loadmat(path, squeeze_me=True, variable_names=names)
+    mat_indices = []
+    data = {}
+
+    mat = hdf5storage.loadmat(path)
+    shape = mat['chan_names'].shape
+
+    if shape[0] == 1 and shape[1] >= 1:
+        for name in mat['chan_names'][0]:
+            mat_indices.append((str(name[0][0])))
+    elif shape[0] >= 1 and shape[1] == 1:
+        for name in mat['chan_names']:
+            mat_indices.append((str(name[0][0][0])))
+    else:
+        raise KeyError("File does not contain a channel name vector: %s" % path)
+
+    for name in names:
+        index_pos = mat_indices.index(name)
+        data[name] = mat['data'][index_pos]
+
     return data
 
 
-def _datenums_to_datetime(timearr):
-    """
-    Convert array of MATLAB datenum floats to array of datetime objects.
-
-    Parameters
-    ----------
-    timearr: array_like or float
-
-    Returns
-    -------
-    array
-        Array of datetime objects, same shape as input array.
-    """
-    def convert(dn):
-        # ref: https://stackoverflow.com/questions/13965740/converting-matlabs-datenum-format-to-python
-        _dtg = datetime.fromordinal(int(dn)) + timedelta(days=dn % 1) - timedelta(days=366)
-        return _dtg
-
-    # return quickly if float (not array) is given
-    if np.ndim(timearr) == 0:
-        return convert(timearr)
-
-    # convert array
-    timearr = np.asarray(timearr)
-    was_shape = timearr.shape
-    timearr = timearr.flatten()
-    dtarray = np.array([convert(t) for t in timearr])
-    return dtarray.reshape(was_shape)
+if __name__ == '__main__':
+    path = os.path.join('C:\\Users', 'ebg', 'OneDrive - SevanSSP AS', 'Projects', 'Cambo', 'ModelTest', 'CE21130.mat')
+    tname, names = read_names(path)
+    data = read_data(path, names)
+    x1 = data[names[0]]  # first data series
