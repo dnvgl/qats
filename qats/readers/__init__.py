41b2fe19af37c99295b266866bd3ad816b029501
tovop <per.voie@gmail.com>
2019-11-13 19:52:51 +0100

    Alias import of `sintef_mat` module as `matlab` to ensure backwards compatibility.

--- qats/readers/__init__.py
+++ qats/readers/__init__.py
@@ -5,6 +5,7 @@ Sub-package with readers for various file formats.
 from . import csv
 from . import direct_access
 from . import sintef_mat
+from . import sintef_mat as matlab  # for backwards compatibility
 from . import other
 from . import sima
 from . import sima_h5
