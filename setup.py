c7eef4ac7f4de0c00df54d5a22fa18d12455a41d
Erling Neerland Lone <erling.lone@ntnu.no>
2020-12-03 13:30:11 +0100

    setup.py: corrected npTDMS requirement

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
         "numpy>=1,<2",
         "scipy>=1,<2",
         "matplotlib>=3,<4",
-        "npTDMS>=0.24,<1",
+        "npTDMS>=1,<2",
         "h5py>=2.7,<4",
         "QtPy>=1,<2",
         "pandas>=1,<2",
