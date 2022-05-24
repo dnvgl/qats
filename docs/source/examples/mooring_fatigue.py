3f419da0809ee701463793e9c0acd729bad5a33b
Per Voie <per.voie@gmail.com>
2022-05-24 14:57:39 +0200

    Revert commit moved to different branch.
    Changes to the fatigue capacity model, relevant tests and docs are moved to the `new-sn-model` branch.

--- docs/source/examples/mooring_fatigue.py
+++ docs/source/examples/mooring_fatigue.py
@@ -1,17 +1,17 @@
 """
-Calculate mooring line fatigue using Rainflow counting and a traditional S-N curve.
+Calculate mooring line fatigue.
 """
 import os
+import numpy as np
 from math import pi
 from qats import TsDB
-from qats.fatigue.sn import SNCurve
-
+from qats.fatigue.sn import SNCurve, minersum
 
 # load time series
 db = TsDB.fromfile(os.path.join("..", "..", "..", "data", "simo_p_out.ts"))
 
 # initiate SN-curve: DNVGL-OS-E301 curve for studless chain
-sncurve = SNCurve(name="Studless chain OS-E301", m=3.0, b0=10.778)
+sncurve = SNCurve(name="Studless chain OS-E301", m1=3.0, a1=6e10)
 
 # Calculate fatigue damage for all mooring line tension time series (kN)
 for ts in db.getl(names='tension_*_qs'):
@@ -22,11 +22,11 @@ for ts in db.getl(names='tension_*_qs'):
     ranges, _, counts = cycles.T
 
     # calculate cross section stress cycles (shown here: 118mm studless chain, with unit [kN] for tension cycles)
-    area = 2. * pi * (118. / 2.) ** 2.  # mm^2
-    ranges = 1e3 * ranges / area        # MPa
+    area = 2. * pi * (118. / 2.) ** 2.          # mm^2
+    ranges = [r * 1e3 / area for r in ranges]   # MPa
 
     # calculate fatigue damage from Palmgren-Miner rule (SCF=1, no thickness correction)
-    damage, _ = sncurve.minersum(ranges, counts)
+    damage = minersum(ranges, counts, sncurve)
 
     # print summary
     print(f"{ts.name}:")
